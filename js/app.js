2b30ed962118023e995d9641ed43ef9a91954cb4
Allen Wu <allenwu1018@gmail.com>
2015-09-28 21:29:23 +0800

    Lesson 1: Buy Shoes Site Layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+function makeCartScrollNicely() {
+  var cart = document.querySelector('.cart');
+  var productList = document.querySelector('.products');
+  Ps.initialize(cart);
+  Ps.initialize(productList);
+}
+
 window.onload = function() {
   console.log("page loaded");
 }
